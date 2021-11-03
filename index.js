d61e1a2e68be65b16abfc897ae95c5c0b319df48
Glen (G.J.) Critchlow <56050959+gjcritchlow21@users.noreply.github.com>
2021-11-02 21:07:13 -0400

    first 3 stories completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*----------------Boiler Plate-------------*/
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -6,6 +7,17 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
+/*---------------Global------------*/
+let min = 1
+let max = 100
+
+/*------------Function Block----------*/
+
+function randoNumb(min,max) {
+  let range = max - min + 1
+  return min + Math.floor(Math.random() * range)
+}
+
 
 start();
 
@@ -13,6 +25,21 @@ async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
+  let compGuess = randoNumb(min,max)
+  let answer = await ask('Is your number ' + compGuess + "?")
+  if(answer === "y") {
+    console.log("Whoo hoo! Skynet wins again!!")
+    process.exit()
+  } else {
+    while(answer !== "y") {
+      let hiLow = await ask("Is it higher or lower? ")
+      console.log(hiLow)
+    }
+  }
+
+
+
+
   // Now try and complete the program.
-  process.exit();
+  //process.exit();
 }
